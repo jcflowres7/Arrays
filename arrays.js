2cb4d5abdd7f624d5ec588a2a6fa8450e79d21f5
Juan Flores <jcflowres7@gmail.com>
2021-02-13 19:39:11 -0500

    commit

---
+++ arrays.js
@@ -0,0 +1,25 @@
+for(var i = 1000; i >=0; i -=2) {
+    console.log(i)
+}
+
+
+
+for(var i = 0; i <= 10000; i++) {
+}
+
+if(i = 2500) {
+    alert("A quarter of the way there");
+}
+if(i = 5000) {
+    alert("Halfway there!");
+} 
+if(i = 10000) {
+    alert("The loop is done") 
+}
+
+const favoriteTVshows = ["The Office", "Person of interest", "Bones", "The Blacklist", "NCIS"]; 
+
+for(var i = 0; i < favoriteTVshows.length; i++) { 
+    console.log("My #" + (i+1), "favorite TV show is", favoriteTVshows  [i]);
+}
+
